38818bdac4ac9aae55894be288f1dbadbcdafeea
moreshwarnabar <mrnabar@gmail.com>
2024-03-30 08:08:14 -0700

    added a new route to the server

--- server/index.js
+++ server/index.js
@@ -2,11 +2,15 @@ import express from 'express';
 import * as dotenv from 'dotenv';
 import cors from 'cors';
 
+import dalleroutes from './routes/dalle.routes.js';
+
 dotenv.config();
 
 const app = express();
 app.use(cors());
 app.use(express.json({ limig: '50mb' }));
+
+app.use('/api/v1/dalle', dalleroutes);
 
 app.get('/', (req, res) => {
   res.status(200).json({ message: 'Hello from DALL.E!' });
